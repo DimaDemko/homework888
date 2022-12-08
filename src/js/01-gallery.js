92bf0535b3f61ed5a461a8815e4444210b8da40c
DimaDemko <idmitriy4g@gmail.com>
2022-12-08 22:57:10 +0200

    task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+const galleryDiv = document.querySelector('.gallery');
+const galleryHtml = addGalleryItemsHtml(galleryItems);
+galleryDiv.insertAdjacentHTML('beforeend', galleryHtml);
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+  /* options */
+});
+// galleryDiv.addEventListener('click', clickOnimageLink);
+
+// function clickOnimageLink(event) {
+//   event.preventDefault();
+//   if (!event.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   basicLightbox
+//     .create(
+//       `
+//     <img src=${event.target.dataset.source} width="800" height="600">
+// `
+//     )
+//     .show();
+// }
+
+function addGalleryItemsHtml(input) {
+  return input
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+const wind = document.querySelector('#vimeo-player');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let timeSeonds = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+).seconds;
+
+player.setCurrentTime(timeSeonds);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(data) {
+  let time = JSON.stringify(data);
+  localStorage.setItem('videoplayer-current-time', time);
+  console.log('onplay');
+}
+console.log(player);
+
+wind.addEventListener('timeupdate', () => console.log('hsjfd'));
